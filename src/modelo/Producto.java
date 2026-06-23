c244914482c77951e75db66816b7f1828b97b062
fernanshay <shayfernandog@gmail.com>
2026-06-23 00:35:38 -0500

    Creación de la clase Producto con encapsulamiento y toString

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,7 +1,55 @@
 package modelo;
 
-// ID, nombre, precio, prioridadEnvio, etc.
-
 public class Producto {
-    
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    // Constructor:
+    public Producto(String codigo, String nombre, double precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // Getters y Setters:
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // Print producto como texto:
+    @Override
+    public String toString() {
+        return "[" + codigo + "]" + nombre + " - S/" + precio + " (Stock: " + stock + ")";
+    }
 }

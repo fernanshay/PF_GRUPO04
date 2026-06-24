a145731bbd0573a2fab6e111fa318db23b1ae0dc
Bryan0005 <bryanjuegowow1@gmail.com>
2026-06-23 19:58:48 -0500

    Update NodoPila.java

--- src/modelo/NodoPila.java
+++ src/modelo/NodoPila.java
@@ -1,7 +1,27 @@
 package modelo;
 
-// (Contiene un Producto y el puntero 'siguiente')
-
+// (Contiene un Producto y el puntero siguiente)
 public class NodoPila {
+    private Producto producto;
+    private NodoPila sgte;
 
+    // Constructor:
+    public NodoPila(Producto producto) {
+        this.producto = producto;
+        this.sgte = null;
+    }
+
+    // Getters y Setters:
+    public Producto getProducto() {
+        return producto;
+    }
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    public NodoPila getSgte() {
+        return sgte;
+    }
+    public void setSgte(NodoPila sgte) {
+        this.sgte = sgte;
+    }
 }

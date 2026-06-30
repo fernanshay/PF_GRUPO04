899a36e7c31f173d19a9860d34c432c63db0a2d4
Sup <nsa.zapatamathiass@gmail.com>
2026-06-30 00:34:01 -0500

    Add files via upload

--- src/modelo/NodoCola.java
+++ src/modelo/NodoCola.java
@@ -3,5 +3,29 @@ package modelo;
 // Contiene un Producto, la prioridad y el puntero 'siguiente'
 
 public class NodoCola {
+    private Producto producto;
+    private cNodo sgte;
 
+    // Constructor:
+    public cNodo(Producto producto) {
+        this.producto = producto;
+        this.sgte = null;
+    }
+
+    // Getters y Setters:
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public cNodo getSgte() {
+        return sgte;
+    }
+
+    public void setSgte(cNodo sgte) {
+        this.sgte = sgte;
+    }    
 }
